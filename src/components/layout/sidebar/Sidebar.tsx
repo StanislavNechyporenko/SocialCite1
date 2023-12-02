a332721c4414745fd0d6ba5691b2537197f0f156
Stanislav <iadomaa97@gmail.com>
2023-12-02 16:43:26 +0100

    1

---
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -0,0 +1,27 @@
+import { Sun, Users2, Phone, MessagesSquare, Settings } from 'lucide-react';
+import Image from 'next/image';
+import styles from './Sidebar.module.scss';
+import Link from 'next/link';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <Image src="/arc.svg" alt="" width={50} height={50} />
+      <div>
+        <Link href="/friends">
+          <Users2 />
+        </Link>
+        <Link href="/call">
+          <Phone />
+        </Link>
+        <Link href="/chats">
+          <MessagesSquare />
+        </Link>
+        <Link href="/settings">
+          <Settings />
+        </Link>
+      </div>
+      <Sun />
+    </aside>
+  );
+}
